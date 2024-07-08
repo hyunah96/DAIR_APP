50dd3c9003902bac17cdfbf1fa0a33002884eb89
hyunahkim <ha.kim@d-air.co.kr>
2024-07-08 11:15:25 +0900

    commit

--- app/src/main/java/com/dji/dair/internal/repository/FTPConnectionManager.java
+++ app/src/main/java/com/dji/dair/internal/repository/FTPConnectionManager.java
@@ -2,6 +2,8 @@ package com.dji.dair.internal.repository;
 
 import android.os.Environment;
 import android.util.Log;
+
+import dji.v5.common.callback.CommonCallbacks;
 import dji.v5.manager.SDKManager;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
@@ -11,6 +13,7 @@ import org.greenrobot.eventbus.Subscribe;
 import java.io.File;
 
 import dji.v5.manager.SDKManager;
+import dji.v5.manager.interfaces.IMediaManager;
 import dji.v5.manager.datacenter.media.MediaManager;
 import dji.v5.ux.core.base.DJISDKModel;
 import dji.v5.ux.visualcamera.storage.SDCardInsertedEvent;
@@ -19,10 +22,11 @@ import dji.v5.ux.visualcamera.storage.SDCardRemovedEvent;
 public class FTPConnectionManager {
 
     private FTPClient ftpClient;
-    private String server = "192.168.10.200";
+    private String server = "192.168.20.40";
     private int port = 21;
     private String user = "hakim";
     private String password = "kgb0563";
+    private boolean ftp_connected = false;
     String sdCardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
 
     public FTPConnectionManager() {
@@ -33,50 +37,32 @@ public class FTPConnectionManager {
 //        this.ftpClient = new FTPClient();
         EventBus.getDefault().register(this);
     }
-//    private boolean connectFTP() {
-//        try {
-//            Log.d("test","connectFTP");
-//            try{
-//            ftpClient.connect(server, port);
-////            if(ftpClient.login(user, password)) {
-////                Log.d("test", "ftpClient login"+ sdCardPath);
-////                int reply = 0;
-////                reply = ftpClient.getReplyCode();
-////                Log.d("test","reply : "+ reply);
-////                //ftpClient.enterLocalActiveMode();
-////                //ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-////                }
-//            }
-//            catch (Exception e){
-//                Log.d("test","connectFTP error : "+ e);
-//            }
-//
-//            return true;
-//        }
-//        catch (Exception e){
-//            return false;
-//        }
-//    }
+
 
     public boolean connectFTP() {
-        try {
-            ftpClient = new FTPClient();
-                ftpClient.connect(server, port);
-                Log.d("test", "ftpClient.getReplyCode(): " + ftpClient.getReplyCode());
-                if (ftpClient.login(user, password)) {
-                    //var product = SDKManager.getInstance().getProductCategory();
-                    ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-                    // 230 : 로그인 성공
-                    Log.d("test", "connectFTP login 성공 " + ftpClient.getReplyCode());
-                    return true;
-                } else {
-                    Log.d("test", "connectFTP login 실패");
-                    return false;
+
+            if(!ftp_connected)
+                try {
+                    ftpClient = new FTPClient();
+                    ftpClient.connect(server, port);
+                    Log.d("test", "ftpClient.getReplyCode(): " + ftpClient.getReplyCode());
+                    if (ftpClient.login(user, password)) {
+                        MediaManager mediaManager =
+                        //ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+                        // 230 : 로그인 성공
+                        Log.d("test", "connectFTP login 성공 " + ftpClient.getReplyCode());
+                        ftp_connected = true;
+                        return true;
+                    } else {
+                        Log.d("test", "connectFTP login 실패");
+                        ftp_connected = false;
+                        return false;
                 }
         } catch (Exception e) {
             Log.e("test", "connectFTP Error : ", e);
             return false;
         }
+        return false;
     }
 
 
